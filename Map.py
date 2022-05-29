1085abe9f51b4f09bdad074e371e25bc06109c5e
nureles16 <99633204+nureles16@users.noreply.github.com>
2022-05-29 12:46:51 +0600

    file is changed(element)

---
+++ Map.py
@@ -0,0 +1,67 @@
+from Cell import Cell
+
+
+class Map:
+    def __init__(self, rows, cols):
+        self._rows = rows
+        self._cols = cols
+        self._cells = [[Cell() for x in range(cols)] for y in range(rows)]
+
+    # TODO: rows getter
+    @property
+    def rows(self):
+        return self._rows
+
+    # TODO: cols getter
+    @property
+    def cols(self):
+        return self._cols
+
+    def get_cell(self, row, col):
+        # TODO: check whether the position is out of boundary
+        #       if not, return the cell at (row, col)
+        #       return None otherwise
+        if (row < 0 or row >= self._rows or col < 0 or col >= self._cols):
+            print("\033[1;31;46mNext move is out of boundary!\033[0;0m")
+            return None
+        else:
+            # return a cell
+            return self._cells[row][col]
+        # END TODO
+
+    def build_cell(self, row, col, cell):
+        # TODO: check whether the position is out of boundary
+        #       if not, add a cell (row, col) and return True
+        #       return False otherwise
+        if (row < 0 or row >= self._rows or col < 0 or col >= self._cols):
+            print(
+                "\033[1;31;46mThe position (%d, %d) is out of boundary!\033[0;0m" % (row, col))
+            return False
+        else:
+            # return a cell
+            self._cells[row][col] = cell
+            return True
+        # END TODO
+
+    # return a list of cells which are neighbours of cell (row, col)
+    def get_neighbours(self, row, col):
+        return_cells = []
+        # TODO: return a list of neighboring cells of cell (row, col)
+        for i in range(max(0, row-1), min(row+1, self._rows-1)+1):
+            for j in range(max(0, col-1), min(col+1, self._cols-1)+1):
+                return_cells.append(self._cells[i][j])
+        return return_cells
+        # END TODO
+
+    def display(self):
+        # TODO: print the map by calling diplay of each cell
+        print(end='   ')
+        for i in range(self._cols):
+            print(i, end='     ')
+        print('')
+        for i in range(self._rows):
+            print(i, end=' ')
+            for j in range(self._cols):
+                self._cells[i][j].display()
+            print('\n')
+        # END TODO
---
+++ Volcano.py
@@ -0,0 +1,45 @@
+from select import select
+from Cell import Mountain
+
+
+class Volcano(Mountain):
+    def __init__(self, row, col, freq):
+        Mountain.__init__(self, row, col)
+        self._countdown = freq
+        self._frequency = freq
+        self._color = '\u001b[41m'
+        self._active = True
+
+    # TODO: _active getter
+    @property
+    def active(self):
+        return self._active
+
+    def act(self, map):
+        # TODO: reduce the countdown by 1
+        #       when the countdown is zero, refresh the countdown
+        #       get all objects occupying the neighboring cells
+        #		and update their properties accordingly
+        self._countdown -= 1
+        if(self._countdown == 0):
+            print("\033[1;33;41mVolcano erupts! \033[0;0m")
+            # add game logic
+            self._countdown = self._frequency
+            cells = map.get_neighbours(self._row, self._col)
+            for cell in cells:
+                occ = cell.occupant
+                if(occ != None):
+                    if(occ.name == "Goblin"):
+                        occ.active = False
+                        occ.occupying.remove_occupant()
+                    elif(occ.name == "Player"):
+                        occ.hp = occ.hp - 1
+        # END TODO
+
+    def display(self):
+        # TODO: return a string representing the Volcano
+        # print(self._color, '\033[42m',
+        #       self._countdown, self._color, end='\033[0m  ')
+        print('{0} \033[2;97m{1}{2} \033[0m   '.format(
+            self._color, self._countdown, self._color), end='')
+    # END TODO
